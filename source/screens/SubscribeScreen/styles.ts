fd3def3fedd919e7ed0294006ce9d207c7fd2fc0
Ygor <ygor_sy@hotmail.com>
2021-04-20 01:45:21 -0300

    PS-77 Ajuste no bug de estilo

--- source/screens/SubscribeScreen/styles.ts
+++ source/screens/SubscribeScreen/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Title = styled.h1`
   color: red;
   font-size: 50px;
-  background-color: ${({ theme }) => theme.colors.background}};
+  background-color: ${({ theme }) => theme.colors.background};
   width: 400px;
   height: 500px;
   margin: 0;
