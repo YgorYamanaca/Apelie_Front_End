54afe83ebdc1498395c4d881efc1a3d5e427ca40
Ygor <ygor_sy@hotmail.com>
2021-04-27 00:51:05 -0300

    hotfix - SEO

--- source/components/commons/SEO/index.tsx
+++ source/components/commons/SEO/index.tsx
@@ -10,20 +10,16 @@ const SEO: React.FC<ISEO> = ({ pageTitle, url }) => {
 
   return (
     <Head>
-      <title>{title}</title>
       <meta name="title" content={title} />
-      <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
       <meta name="description" content={description} />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io/" />
       <meta property="og:url" content={urlBase} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://metatags.io/" />
       <meta property="twitter:url" content={urlBase} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
