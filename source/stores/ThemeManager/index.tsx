d07a6e567560fb95c8f7ae35891c99e4ff974625
Ygor <ygor_sy@hotmail.com>
2021-04-27 01:05:15 -0300

    test

--- source/stores/ThemeManager/index.tsx
+++ source/stores/ThemeManager/index.tsx
@@ -1,9 +1,8 @@
 import React, {
-  createContext, useState, useCallback, useMemo, useEffect,
+  createContext, useState, useEffect,
 } from 'react';
 import GlobalStyle from '@/components/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
-import lightPalette from '@/themes/colors/LightPalette';
 import shadow from '@/themes/shadow';
 import typography from '@/themes/typography';
 import darkPalette from '@/themes/colors/DarkPalette';
@@ -14,6 +13,7 @@ export const AppThemeContext = createContext<Function>(() => {});
 
 const ApelieThemeProvider: React.FC = ({ children }) => {
   const [apelieTheme, setApelieTheme] = useState<ITheme>();
+  // eslint-disable-next-line max-len
   // const actualTheme = useMemo(() => (apelieTheme === 'Dark' ? darkPalette : lightPalette), [apelieTheme]);
 
   useEffect(() => {
