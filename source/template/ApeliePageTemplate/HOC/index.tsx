accb7e8def6d34e7c8f15c896ba1a48d34b7f7c4
Ygor Yamanaca <ygor_sy@hotmail.com>
2021-09-24 00:57:53 -0300

    fix

--- source/template/ApeliePageTemplate/HOC/index.tsx
+++ source/template/ApeliePageTemplate/HOC/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import IApeliePageTemplate from '@/types/interfaces/interface-apelie-page-template';
 import ApeliePageTemplate from '../index';
-import ApelieGlobalProvider from '../provider/index';
 
 interface IApeliePageHOC {
   apelieTemplateProps: IApeliePageTemplate;
